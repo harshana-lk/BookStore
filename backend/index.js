3161992eb1c3ab27534069296924cf40f3a8b42d
Harshana Jayaweera <harshuonlive@gmail.com>
2024-02-10 15:02:12 +0530

    created route for save a book

--- backend/index.js
+++ backend/index.js
@@ -1,12 +1,39 @@
 import express, { request, response } from "express";
 import { PORT, mongoDBURL } from "./config.js";
 import mongoose from "mongoose";
+import { Book } from "./models/bookModels.js";
 
 const app = express();
 
 app.get("/", (request, response) => {
   console.log(request);
   return response.status(234).send("Hello World");
+});
+
+// Route for save a new book
+app.post("/books", async (request, response) => {
+  try {
+    if (
+      !request.body.title ||
+      !request.body.author ||
+      !request.body.publishYear
+    ) {
+      return response.status(400).send({
+        message: "Send all required fields: title,author,publishYear",
+      });
+    }
+    const newBook = {
+      title: request.body.title,
+      author: request.body.author,
+      publishYear: request.body.publishYear,
+    };
+
+    const book = await Book.create(newBook);
+    return response.status(201).send(book);
+  } catch {
+    console.log(error.message);
+    response.status(500).send({ message: error.message });
+  }
 });
 
 mongoose
